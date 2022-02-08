b7854aa8b6d99f3dbaab0c5a1da561fbfc37f6a6
Yousef <gheibiyousef2000@gmail.com>
2022-02-08 20:09:45 +0330

    implement @Input()

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-star',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StarComponent implements OnInit {
 
-  rating: number = 2.3;
+  @Input() rating: number = 2.3;
   width: number = 90;
   constructor() { }
 
